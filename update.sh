926cbcb01cc7a3cdc48879f72db349ff4f75c386
Mattia Basone <mattia.basone@gmail.com>
2019-04-12 17:42:30 +0200

    Added code sniffer & phpmd

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# TODO: improve this script
+
+IMAGE_NAME="mattiabasone/php"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${DIR}/7.1/cli/
+docker build . -t ${IMAGE_NAME}:7.1-cli
+docker push ${IMAGE_NAME}:7.1-cli
+
+cd ${DIR}/7.2/cli/
+docker build . -t ${IMAGE_NAME}:7.2-cli
+docker push ${IMAGE_NAME}:7.2-cli
+
+cd ${DIR}/7.3/cli/
+docker build . -t ${IMAGE_NAME}:7.3-cli
+docker push ${IMAGE_NAME}:7.3-cli
