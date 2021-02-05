faab8af373358dd60ee81627cb14cac1490e15ae
fabianoroberto <fabiano.roberto@ped.technology>
2021-02-05 16:27:12 +0100

    fix(docker): fix dockerfile for php 7.4 and 8.0

--- update.sh
+++ update.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-# TODO: improve this script
 
 IMAGE_NAME="jointly/php"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
-cd ${DIR}/7.3/cli/
-docker build . -t ${IMAGE_NAME}:7.3-cli
-docker push ${IMAGE_NAME}:7.3-cli
-
-docker push ${IMAGE_NAME}:7.4-cli
-cd ${DIR}/7.4/cli/
-docker build . -t ${IMAGE_NAME}:7.4-cli
-
-docker push ${IMAGE_NAME}:8.0-cli
-cd ${DIR}/8.0/cli/
-docker build . -t ${IMAGE_NAME}:8.0-cli
+for VERSION in *; do
+    if [ -d "${VERSION}" ]; then
+        echo "${VERSION}" # your processing here
+        cd ${DIR}/${VERSION}/cli/
+        docker build . -t ${IMAGE_NAME}:${VERSION}-cli
+        docker push ${IMAGE_NAME}:${VERSION}-cli
+    fi
+done
