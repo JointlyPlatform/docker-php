1e11d69ade65a6d1f9ed399ee0eefc85dd9e7f6c
fabianoroberto <fabiano.roberto@ped.technology>
2021-02-05 14:56:31 +0100

    feat(docker): remove old cli version of php (5.6, 7.0, 7.2), add new (8.0) and fix gd with jpeg

--- update.sh
+++ update.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 # TODO: improve this script
 
-IMAGE_NAME="mattiabasone/php"
+IMAGE_NAME="jointly/php"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
-cd ${DIR}/5.6/cli/
-docker build . -t ${IMAGE_NAME}:5.6-cli
-docker push ${IMAGE_NAME}:5.6-cli
-
-cd ${DIR}/7.0/cli/
-docker build . -t ${IMAGE_NAME}:7.0-cli
-docker push ${IMAGE_NAME}:7.0-cli
-
-cd ${DIR}/7.1/cli/
-docker build . -t ${IMAGE_NAME}:7.1-cli
-docker push ${IMAGE_NAME}:7.1-cli
-
-cd ${DIR}/7.2/cli/
-docker build . -t ${IMAGE_NAME}:7.2-cli
-docker push ${IMAGE_NAME}:7.2-cli
 
 cd ${DIR}/7.3/cli/
 docker build . -t ${IMAGE_NAME}:7.3-cli
 docker push ${IMAGE_NAME}:7.3-cli
 
+docker push ${IMAGE_NAME}:7.4-cli
 cd ${DIR}/7.4/cli/
 docker build . -t ${IMAGE_NAME}:7.4-cli
-docker push ${IMAGE_NAME}:7.4-cli
+
+docker push ${IMAGE_NAME}:8.0-cli
+cd ${DIR}/8.0/cli/
+docker build . -t ${IMAGE_NAME}:8.0-cli
