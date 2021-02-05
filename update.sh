d8f59c1535d62958a1710d42eb30a981b7da345f
fabianoroberto <fabiano.roberto@ped.technology>
2021-02-05 16:29:46 +0100

    fix(docker): fix update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for VERSION in *; do
     if [ -d "${VERSION}" ]; then
         echo "${VERSION}" # your processing here
+
         cd ${DIR}/${VERSION}/cli/
         docker build . -t ${IMAGE_NAME}:${VERSION}-cli
         docker push ${IMAGE_NAME}:${VERSION}-cli
+
+        cd -
     fi
 done
